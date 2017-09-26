a6889c1f8509d26cb2e94f0b1f4c5db2fafa9c67
Héctor Bállega <hectorballega@gmail.com>
2017-09-26 14:44:49 +0200

    Expire user session if user is inactive for X minutes

--- lab2-part1/index.php
+++ lab2-part1/index.php
@@ -1,5 +1,35 @@
 <?php
     session_start();
+    //Start our session.
+  
+    //Expire the session if user is inactive for 30
+    //minutes or more.
+    $expireAfter = 1;
+  
+    //Check to see if our "last action" session
+    //variable has been set.
+    if(isset($_SESSION['last_action'])){
+     
+         //Figure out how many seconds have passed
+         //since the user was last active.
+         $secondsInactive = time() - $_SESSION['last_action'];
+     
+         //Convert our minutes into seconds.
+         $expireAfterSeconds = $expireAfter * 60;
+     
+         //Check to see if they have been inactive for too long.
+         if($secondsInactive >= $expireAfterSeconds){
+           //User has been inactive for too long.
+           //Kill their session.
+           session_unset();
+           session_destroy();
+           Header('Location: '.$_SERVER['PHP_SELF']);
+        }
+    }
+  
+    //Assign the current timestamp as the user's
+    //latest activity
+    $_SESSION['last_action'] = time();
  ?>
 
 
