53eb2271eaacff8e8692924aed2d1e3ea725db64
Héctor Bállega <hectorballega@gmail.com>
2017-09-27 16:40:04 +0200

    Keep the session when the user closes the browser.

--- lab2-part1/addToFavorites.php
+++ lab2-part1/addToFavorites.php
@@ -2,6 +2,8 @@
 <html>
 
 <?php
+ini_set('session.cookie_lifetime', 60 * 60 * 24 * 365);
+ini_set('session.gc-maxlifetime', 60 * 60 * 24 * 365);
   session_start();
 
   if(!isset($_SESSION['username']))
